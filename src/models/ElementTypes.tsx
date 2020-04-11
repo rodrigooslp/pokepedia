75df84fef596a8c00dc710b6488164bf785b5d1b
Rodrigo Lopes <rodrigooslp@gmail.com>
2020-04-11 10:02:41 -0300

    Criação do model de ElementTypes

---
+++ src/models/ElementTypes.tsx
@@ -0,0 +1,19 @@
+export type ElementTypes =
+  'normal' |
+  'fighting' |
+  'flying' |
+  'poison' |
+  'ground' |
+  'rock' |
+  'bug' |
+  'ghost' |
+  'steel' |
+  'fire' |
+  'water' |
+  'grass' |
+  'electric' |
+  'psychic' |
+  'ice' |
+  'dragon' |
+  'dark' |
+  'fairy';
