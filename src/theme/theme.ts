c1bd364399cb840c9f6a6e0909ff1d7d5bef0f35
Rodrigo Lopes <rodrigooslp@gmail.com>
2020-04-11 20:10:54 -0300

    Ajuste nos breakpoints do tema

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -8,5 +8,14 @@ export const theme = createMuiTheme({
       main: blue[400],
       dark: blue[500]
     }
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 800,
+      md: 1280,
+      lg: 1650,
+      xl: 1920
+    }
   }
 });
