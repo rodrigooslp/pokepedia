e53b31d8ba12282e41fc42b5d09c03ccdcc1bdf2
Rodrigo Lopes <rodrigooslp@gmail.com>
2020-04-11 11:02:49 -0300

    Limpeza no atom de PokemonCover

--- src/components/atoms/PokemonCover/PokemonCover.tsx
+++ src/components/atoms/PokemonCover/PokemonCover.tsx
@@ -1,14 +1,7 @@
 import React, { FC } from 'react';
-// import styled from 'styled-components';
 
 type PokemonCoverProps = {
   url: string;
 };
 
-// const StyledPokemonCover = styled.img`
-//   width: 48px;
-//   height: 48px;
-//   cursor: pointer;
-// `;
-
-export const PokemonCover: FC<PokemonCoverProps> = ({ url, ...props }) => <img src={url} alt='cover'/>;
+export const PokemonCover: FC<PokemonCoverProps> = ({ url, ...props }) => <img src={url} alt='cover' {...props}/>;
