a5b6df1408d704565b9402f5677825ebd64268e4
Rodrigo Lopes <rodrigooslp@gmail.com>
2020-04-11 10:29:15 -0300

    Criação do atom de PokemonName

---
+++ src/components/atoms/PokemonName/PokemonName.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { PokemonName } from './PokemonName';
+
+export default {
+  title: 'atoms/PokemonName'
+}
+
+export const Default = () => <PokemonName text='Squirtle' />;
