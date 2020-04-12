c72950e6da94299f609385e21de23c799a765c13
Rodrigo Lopes <rodrigooslp@gmail.com>
2020-04-11 22:26:20 -0300

    Correção no tamanho da imagem de cover

--- src/components/atoms/PokemonCover/PokemonCover.tsx
+++ src/components/atoms/PokemonCover/PokemonCover.tsx
@@ -1,7 +1,12 @@
 import React, { FC } from 'react';
+import styled from 'styled-components';
 
 type PokemonCoverProps = {
   url: string;
 };
 
-export const PokemonCover: FC<PokemonCoverProps> = ({ url, ...props }) => <img src={url} alt='cover' {...props}/>;
+const StyledImg = styled.img`
+  max-height: 160px;
+`;
+
+export const PokemonCover: FC<PokemonCoverProps> = ({ url, ...props }) => <StyledImg src={url} alt='cover' {...props}/>;
