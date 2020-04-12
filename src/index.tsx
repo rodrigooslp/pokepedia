b6190ca3806c3ccc79d19519e377c1dd5ddd566d
Rodrigo Lopes <rodrigooslp@gmail.com>
2020-04-12 00:03:03 -0300

    Remoção de referência ao css removido

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.css';
 import { ThemeProvider, CssBaseline, StylesProvider } from '@material-ui/core';
 import { theme } from 'theme';
 import { Home } from 'pages';
