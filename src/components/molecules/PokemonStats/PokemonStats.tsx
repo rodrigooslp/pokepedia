596674d94f73ca7457f214f8d813ef73f3fa575a
Rodrigo Lopes <rodrigooslp@gmail.com>
2020-04-11 22:25:53 -0300

    Correção na margem do PokemonStats

--- src/components/molecules/PokemonStats/PokemonStats.tsx
+++ src/components/molecules/PokemonStats/PokemonStats.tsx
@@ -8,16 +8,33 @@ type PokemonStatsProps = {
   stats: Stats
 };
 
-const StyledStatItem = styled(StatItem)`
+const StyledStatItem = styled.div`
   margin-bottom: 5px;
 `;
 
 export const PokemonStats: FC<PokemonStatsProps> = ({ stats, ...props }) =>
   <Box {...props}>
-    <StyledStatItem name='HP' value={stats.hp} />
-    <StyledStatItem name='Attack' value={stats.attack} />
-    <StyledStatItem name='Defense' value={stats.defense} />
-    <StyledStatItem name='Speed' value={stats.speed} />
-    <StyledStatItem name='Sp. Attack' value={stats.spAttack} />
-    <StyledStatItem name='Sp. Defense' value={stats.spDefense} />
+    <StyledStatItem>
+      <StatItem name='HP' value={stats.hp} />
+    </StyledStatItem>
+
+    <StyledStatItem>
+      <StatItem name='Attack' value={stats.attack} />
+    </StyledStatItem>
+
+    <StyledStatItem>
+      <StatItem name='Defense' value={stats.defense} />
+    </StyledStatItem>
+
+    <StyledStatItem>
+      <StatItem name='Speed' value={stats.speed} />
+    </StyledStatItem>
+
+    <StyledStatItem>
+      <StatItem name='Sp. Attack' value={stats.spAttack} />
+    </StyledStatItem>
+
+    <StyledStatItem>
+      <StatItem name='Sp. Defense' value={stats.spDefense} />
+    </StyledStatItem>
   </Box>;
